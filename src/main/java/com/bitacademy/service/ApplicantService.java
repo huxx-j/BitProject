22c7b3b1596cd78d78b7825185826f18c72dcfc6
ll0513ll <ll0513ll@naver.com>
2018-08-06 18:24:22 +0900

    지원자관리 코드 정리

--- src/main/java/com/bitacademy/service/ApplicantService.java
+++ src/main/java/com/bitacademy/service/ApplicantService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.bitacademy.dao.ApplicantDao;
 import com.bitacademy.vo.ApplyUserVo;
+import com.bitacademy.vo.CurriculumCateVo;
 import com.bitacademy.vo.CurriculumVo;
 
 @Service
@@ -17,26 +18,31 @@ public class ApplicantService {
 	@Autowired
 	ApplicantDao applicantDao;
 
-	public List<CurriculumVo> getWorkType(){
+	//맨윗줄 업무구분 가져가기
+	public List<CurriculumCateVo> getCurriCateList(){
 
-		return applicantDao.getWorkType();
-	}//맨윗줄 업무구분 가져가기
+		return applicantDao.getCurriCateList();
+	}
 
-	public List<CurriculumVo> getCurriList(String cateName){
+	//맨윗줄 업무구분관련된 수업이름 가져가기
+	public List<CurriculumVo> getCurriList(String curriculumCate_no){
 
-		return applicantDao.getCurriList(cateName);
-	}//맨윗줄 업무구분관련된 수업이름 가져가기
+		return applicantDao.getCurriList(curriculumCate_no);
+	}
 
+	//조회하기 버튼 후 그리드영역 지원자 리스트 가져오기
 	public List<ApplyUserVo> search(int selectValue){
 
 		return applicantDao.search(selectValue);
-	}//조회하기 버튼 후 그리드영역 지원자 리스트 가져오기
+	}
 
+	//선택된 사람의 과거 지원내역 가져가기
 	public List<ApplyUserVo> getAppliedList(int user_no){
 
 		return applicantDao.getAppliedList(user_no);
-	}//선택된 사람의 과거 지원내역 가져가기
+	}
 
+	//선택된 과거 지원내역의 상세내용
 	public ApplyUserVo apply_details(int curriculum_no,int user_no) {
 
 		Map<String,Integer> noMap = new HashMap<String,Integer>();
@@ -44,25 +50,28 @@ public class ApplicantService {
 		noMap.put("user_no", user_no);
 
 		return applicantDao.apply_details(noMap);
-	}//선택된 과거 지원내역의 상세내용
+	}
 
+	//수정버튼 클릭 후 지원내역 수정
 	public int applyUpdate(ApplyUserVo applyVo) {
 
-		String cardPayDay = applyVo.getCardPayDate();
-		String depositPayDay = applyVo.getDepositDate();
-		int year = Integer.parseInt(cardPayDay.substring(0,4));
-		int month = Integer.parseInt(cardPayDay.substring(5, 7));
-		int day = Integer.parseInt(cardPayDay.substring(8,10));
+		System.out.println("업데이트 서비스");
+		String cardPayDate = applyVo.getCardPayDate();
+		String depositDate = applyVo.getDepositDate();
+		int year = Integer.parseInt(cardPayDate.substring(0,4));
+		int month = Integer.parseInt(cardPayDate.substring(5, 7));
+		int day = Integer.parseInt(cardPayDate.substring(8,10));
 		applyVo.setCardPayDate(year+"/"+month+"/"+day);
 
-		/*System.out.println(year+"/"+month+"/"+day);*/
-		year = Integer.parseInt(depositPayDay.substring(0,4));
-		month = Integer.parseInt(depositPayDay.substring(5,7));
-		day = Integer.parseInt(depositPayDay.substring(8,10));
+		System.out.println(year+"/"+month+"/"+day);
+		
+		year = Integer.parseInt(depositDate.substring(0,4));
+		month = Integer.parseInt(depositDate.substring(5,7));
+		day = Integer.parseInt(depositDate.substring(8,10));
 		applyVo.setDepositDate(year+"/"+month+"/"+day);
 
 		return applicantDao.applyUpdate(applyVo);
 
 
-	}//수정버튼 클릭 후 지원내역 수정
+	}
 }
